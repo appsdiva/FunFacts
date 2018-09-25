c5413a9d5d0ab3cac00cfd4658a952d289cc66be
Stacey Smith <Stacey@staceys-macbook.local>
2018-09-25 14:24:04 +0200

    Initial Commit

---
+++ FunFacts/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FunFacts
+//
+//  Created by Stacey Smith on 9/25/18.
+//  Copyright © 2018 AppsDevo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
